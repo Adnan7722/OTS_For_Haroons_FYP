782abf4017ea3e11c422de2d0a091b43e03fe5ef
M Adnan <adnan.muhammad1290@gmail.com>
2025-01-06 02:33:13 +0500

    sadf

--- Frontend/src/service-worker.js
+++
@@ -1,8 +0,0 @@
-// src/service-worker.js
-import { precacheAndRoute } from 'workbox-precaching';
-
-self.__WB_MANIFEST = []; // This will be populated by Workbox during build
-
-precacheAndRoute(self.__WB_MANIFEST);
-
-// Add any additional caching strategies here if needed
